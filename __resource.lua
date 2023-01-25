267807a84cc0e51116b90599956d0a3f59a3b5f8
Knight Development <61920219+coleminer0112@users.noreply.github.com>
2023-01-25 00:50:34 -0500

    update from legacy manifest

--- __resource.lua
+++
@@ -1,9 +0,0 @@
-description "Enable / Disable Blips on map and names on head."
-
-client_scripts{
-  "client.lua"
-}
-
-server_scripts{ 
-  "server.lua"
-}
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+game "gta5"
+description "Enable / Disable Blips on map and names on head."
+
+client_scripts{
+  "client.lua"
+}
+
+server_scripts{ 
+  "server.lua"
+}
